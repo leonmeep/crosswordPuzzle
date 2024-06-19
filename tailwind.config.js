71b8137ac2a8ad005c00d0d9bb0462d3062d2f45
Leon Birch <leonbirch333@gmail.com>
2024-06-19 11:30:58 +0100

    added a google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        playwritesk: [
+          "Playwrite SK",
+          "cursive",
+          "font-optical-sizing",
+          "auto",
+          "font-StylePropertyMap",
+          "normal",
+        ],
+        roboto: ["Roboto", "sans-serif"], // Add this line
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
